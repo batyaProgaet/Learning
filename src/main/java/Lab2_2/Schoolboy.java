2b840a7ee12595b8d80d29070e7e127d2a549c91
Берестов Сергей <berestov@o-code.ru>
2020-10-05 11:08:15 +0400

    Начало работ по другим заданиям

---
+++ src/main/java/Lab2_2/Schoolboy.java
@@ -0,0 +1,24 @@
+package Lab2_2;
+/*
+
+Написать класс Schoolboy (или Schoolgirl),
+реализующий функциональность, сходную с классом из задания 1.
+Оценки и предметы должны быть представлены полями внутреннего класса Register,
+класс Schoolboy хранит массив Register'ов.
+
+
+*/
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class Schoolboy {
+    public Register[] x;
+
+public class Register{
+    private String lastname;
+    private int[] marks;
+    private String[] subjects;
+}
+
+
+}
